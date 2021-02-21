59444cbc1b4ab46fd32a70f1a894b16e89e07e9b
노수인 <bigpie1367@gmail.com>
2021-02-21 07:21:38 +0000

    initial setting

---
+++ content/blog/JavaScript/test-post.md
@@ -0,0 +1,9 @@
+---
+title: Test Post
+date: 2021-02-21 07:02:66
+category: JavaScript
+thumbnail: { thumbnailSrc }
+draft: false
+---
+
+# For Test
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  title: `D2vlog`,
-  description: `Blog posted about contents that I studied`,
-  author: `bigpie1367`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  title: ``,
+  description: `Blog posted about contents what I learned`,
+  author: `SuIn Roh - bigpie1367`,
+  introduction: `This blog is to record what I have learned`,
+  siteUrl: `https://bigpie1367.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: `https://github.com/bigpie1367`, // Your GitHub account
+    github: `bigpie1367`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
@@ -15,7 +15,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'bigpie1367/bigpie1367-blog', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
