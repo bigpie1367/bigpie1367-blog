8e0351edbbf92dab1193cd357d196b3cac4b8edc
노수인 <bigpie1367@gmail.com>
2021-02-21 13:33:35 +0000

    remove sponsor

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,7 +8,6 @@ import { PostTitle } from '../components/post-title'
 import { PostDate } from '../components/post-date'
 import { PostContainer } from '../components/post-container'
 import { SocialShare } from '../components/social-share'
-import { SponsorButton } from '../components/sponsor-button'
 import { Bio } from '../components/bio'
 import { PostNavigator } from '../components/post-navigator'
 import { Disqus } from '../components/disqus'
@@ -26,7 +25,7 @@ export default ({ data, pageContext, location }) => {
 
   const post = data.markdownRemark
   const metaData = data.site.siteMetadata
-  const { title, comment, siteUrl, author, sponsor } = metaData
+  const { title, comment, siteUrl, author } = metaData
   const { disqusShortName, utterances } = comment
   const { title: postTitle, date } = post.frontmatter
 
@@ -62,9 +61,6 @@ export const pageQuery = graphql`
         comment {
           disqusShortName
           utterances
-        }
-        sponsor {
-          buyMeACoffeeId
         }
       }
     }
